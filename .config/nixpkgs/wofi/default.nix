cdb3b1d300ad8ddc82fc11e9faf377d37a417728
Nadia <nyadiia@pm.me>
2023-05-14 15:05:41 -0500

    Delete .config/nixpkgs directory

--- .config/nixpkgs/wofi/default.nix
+++
@@ -1,5 +0,0 @@
-{
-  imports = [
-    ./wofi-scripts.nix
-  ];
-}
--- .config/nixpkgs/wpaperd.nix
+++
@@ -1,9 +0,0 @@
-{ config, lib, pkgs, ... }:
-
-{
-  home.file.".config/wpaperd/output.conf".text = ''
-    [default]
-    path = "/home/nyadiia/Pictures/Wallpapers/"
-    duration = "1h"
-  '';
-}

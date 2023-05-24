af9bc0618a52a941bedcea07792c66e1bc3fe1c8
Nadia Potteiger <nyadiia@pm.me>
2023-05-24 18:01:09 -0500

    update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,12 +5,13 @@ end
 if status is-login
     and status is-interactive
     # To add a key, set -Ua SSH_KEYS_TO_AUTOLOAD keypath
-    set -Ua SSH_KEYS_TO_AUTOLOAD ~/.ssh/id_ed25519
-    eval $(opam env) > /dev/null
+    set -U SSH_KEYS_TO_AUTOLOAD ~/.ssh/id_ed25519
+    eval $(opam env) >/dev/null
     # To remove a key, set -U --erase 
-SSH_KEYS_TO_AUTOLOAD[index_of_key]
     keychain --eval $SSH_KEYS_TO_AUTOLOAD | source
 end
+
+set fish_greeting
 
 # gpg ssh agent
 # if test -z (pgrep ssh-agent)
@@ -27,18 +28,18 @@ starship init fish | source # load prompt
 zoxide init fish | source
 
 # updated tools
-alias cd 'z'
+alias cd z
 alias ls 'exa --icons'
 ## kitty specific
 alias icat 'kitty +kitten icat'
 
 # faster usage
-alias s 'ssh'
-alias l 'ls'
+alias s ssh
+alias l ls
 alias ll 'ls -al'
 alias la 'ls -a'
-alias cl 'clear'
-alias hx 'helix'
+alias cl clear
+alias hx helix
 
 # alternative paths (cleans up home)
 alias wget 'wget --hsts-file=\'$XDG_DATA_HOME/wget-hsts\''
@@ -50,9 +51,9 @@ alias tig 'git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 
 # user vars
 set -x PATH $PATH $HOME/.spicetify $HOME/bin
-set -x EDITOR 'nvim'
-set -x VISUAL 'nvim'
-set -x MANPATH $MANPATH /usr/local/man 
+set -x EDITOR nvim
+set -x VISUAL nvim
+set -x MANPATH $MANPATH /usr/local/man
 
 # other vars
 source $HOME/.config/fish/conf.d/env.fish

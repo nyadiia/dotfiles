f37e587b585e8b47560e7dc5d0916c60d4e24b5f
Nadia Potteiger <nyadiia@pm.me>
2023-05-25 08:35:05 -0500

    completely change to LazyVim configs

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,26 +1,2 @@
--- disable netrw at the very start of your init.lua (strongly advised)
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-
-require 'plugins'
-require 'lsp'
-require 'colors'
-require('lualine').setup {
-  options = { theme  = "palenight" },
-  extensions = { 'nvim-tree' }
-}
-require('nvim-tree').setup {
-  hijack_cursor = true,
-  update_cwd = true
-}
-require('bufferline').setup()
-
-vim.cmd ([[
-set nu
-set tabstop=2
-set shiftwidth=2
-set expandtab
-nnoremap <C-b> :NvimTreeFocus<CR>
-]])
-
-vim.cmd.colorscheme "catppuccin"
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
